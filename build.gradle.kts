86358aa7eb89a76d097320dde720a0d018cd2059
sigurdgroneng <sigurd.groneng@nav.no>
2024-01-09 16:06:38 +0100

    Add kotlinx

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ val prometeus_version: String by project
 plugins {
     kotlin("jvm") version "1.9.22"
     id("io.ktor.plugin") version "2.3.7"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
 }
 
 group = "dab.poao.nav.no"
