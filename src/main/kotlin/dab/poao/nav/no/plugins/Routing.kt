4501a0445f0dde0a6337ad16028de3eccded1397
sigurdgroneng <sigurd.groneng@nav.no>
2024-01-12 11:03:45 +0100

    Use token from header instead

--- src/main/kotlin/dab/poao/nav/no/plugins/Routing.kt
+++ src/main/kotlin/dab/poao/nav/no/plugins/Routing.kt
@@ -4,9 +4,9 @@ import dab.poao.nav.no.dokark.DokarkClient
 import dab.poao.nav.no.health.healthEndpoints
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import no.nav.security.token.support.v2.TokenValidationContextPrincipal
 import java.lang.IllegalArgumentException
 
 fun Application.configureRouting(dokarkClient: DokarkClient = DokarkClient(environment.config)) {
@@ -17,8 +17,11 @@ fun Application.configureRouting(dokarkClient: DokarkClient = DokarkClient(envir
         }
         authenticate("AzureAD") {
             post("/arkiver") {
-                call.principal<TokenValidationContextPrincipal>()
-                    ?.context?.firstValidToken?.encodedToken
+                call.request.header("Authorization")
+                    ?.split(" ")
+                    ?.lastOrNull()
+//                call.principal<TokenValidationContextPrincipal>()
+//                    ?.context?.firstValidToken?.encodedToken
                     ?.let { dokarkClient.opprettJournalpost(it) }
                     ?: throw IllegalArgumentException("No token found")
             }
