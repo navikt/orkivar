3f9b56abebc5e73c49f4d174593410ce74198936
johannetronstad <johanne.tronstad@nav.no>
2025-06-20 11:40:38 +0200

    Add JDBC URL configuration to application.yaml and update DataSource to use it

--- src/main/kotlin/dab/poao/nav/no/plugins/DataSource.kt
+++ src/main/kotlin/dab/poao/nav/no/plugins/DataSource.kt
@@ -13,18 +13,18 @@ fun configureHikariDataSource(config: ApplicationConfig): DataSource {
     val host = config.property("postgres.host").getString()
     val port = config.property("postgres.port").getString()
     val databaseName = config.property("postgres.database-name").getString()
-    val user = config.property("postgres.user").getString()
-    val pw = config.property("postgres.password").getString()
+//    val user = config.property("postgres.user").getString()
+//    val pw = config.property("postgres.password").getString()
 
     return HikariDataSource(HikariConfig().apply {
         driverClassName = "org.postgresql.Driver"
-        jdbcUrl = "jdbc:postgresql://$host:$port/$databaseName"
+        jdbcUrl = config.property("postgres.jdbc-url").getString()
         maximumPoolSize = 3
         isAutoCommit = true
         initializationFailTimeout = 5000
         minimumIdle = 1
-        username = user
-        password = pw
+//        username = user
+//        password = pw
         validate()
     })
 }
