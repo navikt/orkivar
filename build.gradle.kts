ea8c593c695457633f69132be68fa559c15bf67a
Mads.Lee.Giil <mads.lee.giil@nav.no>
2026-04-07 09:41:53 +0200

    Fjern kode som ble brukt til debug

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ val prometeus_version = "1.15.2"
 
 plugins {
     kotlin("jvm") version "2.2.0"
-    id("io.ktor.plugin") version "3.3.2"
+    id("io.ktor.plugin") version "3.4.0"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.2.0"
 }
 
--- src/main/kotlin/dab/poao/nav/no/plugins/ErrorHandling.kt
+++ src/main/kotlin/dab/poao/nav/no/plugins/ErrorHandling.kt
@@ -15,8 +15,6 @@ fun Application.configureErrorHandling() {
             call.respondText(text = "400: ${cause.message}", status = HttpStatusCode.BadRequest)
         }
         exception<Exception> { call, cause ->
-            cause.printStackTrace()
-            println("HEI, feil")
             call.respondText(text = "500: ${cause.message}", status = HttpStatusCode.InternalServerError)
         }
     }
